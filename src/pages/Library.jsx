65bd0e2fa74adce902252c3398777ca1d68d362c
rafzoio <raphaelzoio@hotmail.com>
2023-07-09 19:37:55 +0100

    Added library page to display songs

---
+++ src/pages/Library.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import List from "../components/List";
+
+const songs = [
+  {
+    id: 1,
+    title: "Dreamy",
+    src: process.env.PUBLIC_URL + "/audio/dreamy.mp3",
+  },
+  {
+    id: 2,
+    title: "Guitar",
+    src: process.env.PUBLIC_URL + "/audio/guitar.mp3",
+  },
+  {
+    id: 3,
+    title: "Piano",
+    src: process.env.PUBLIC_URL + "/audio/piano.mp3",
+  },
+  {
+    id: 4,
+    title: "Piano2",
+    src: process.env.PUBLIC_URL + "/audio/piano2.mp3",
+  },
+  {
+    id: 5,
+    title: "Voice",
+    src: process.env.PUBLIC_URL + "/audio/voice.mp3",
+  },
+];
+
+const Library = () => {
+  return (
+    <div>
+      <List data={songs} />
+    </div>
+  );
+};
+
+export default Library;
