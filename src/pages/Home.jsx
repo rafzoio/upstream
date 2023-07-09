f2d6b1ff0a9dec93fc25bf7217d5c7498bf85497
rafzoio <raphaelzoio@hotmail.com>
2023-07-09 19:38:42 +0100

    Changed font and removed songs from home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,9 @@
 import React from "react";
-import List from "../components/List";
-
-const songs = [
-  {
-    id: 1,
-    title: "Dreamy",
-    src: process.env.PUBLIC_URL + "/audio/dreamy.mp3",
-  },
-  {
-    id: 2,
-    title: "Guitar",
-    src: process.env.PUBLIC_URL + "/audio/guitar.mp3",
-  },
-  {
-    id: 3,
-    title: "Piano",
-    src: process.env.PUBLIC_URL + "/audio/piano.mp3",
-  },
-  {
-    id: 4,
-    title: "Piano2",
-    src: process.env.PUBLIC_URL + "/audio/piano2.mp3",
-  },
-  {
-    id: 5,
-    title: "Voice",
-    src: process.env.PUBLIC_URL + "/audio/voice.mp3",
-  },
-];
 
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl font-karla">Welcome to Upstream</h1>
-      <List data={songs} />
+      <h1 className="text-4xl font-karla">Welcome to Upstream</h1>
     </div>
   );
 };
