feb887aef1025fa86741dcabbe5b7a1025873865
rafzoio <raphaelzoio@hotmail.com>
2023-07-09 19:38:30 +0100

    Changed format to table

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import { ReactComponent as PlayIcon } from "../resources/icons/play.svg";
 
 const List = ({ data }) => {
   const dispatch = useDispatch();
@@ -13,13 +14,31 @@ const List = ({ data }) => {
 
   return (
     <div>
-      <ul>
-        {data.map((song) => (
-          <li key={song.id}>
-            <button onClick={() => handleSongSelect(song)}>{song.title}</button>
-          </li>
-        ))}
-      </ul>
+      <table className="w-fit text-left">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Artist</th>
+            <th>Album</th>
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((song) => (
+            <tr key={song.id}>
+              <td className=" flex flex-row items-center">
+                <button onClick={() => handleSongSelect(song)}>
+                  <PlayIcon className="w-6 fill-gray-500 hover:fill-gray-800" />
+                </button>
+                {song.title}
+              </td>
+              <td className="">Artist</td>
+              <td className="">Album</td>
+              <td className="">2023</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
