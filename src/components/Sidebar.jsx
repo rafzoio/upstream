3f1f1cd68f715e56a8f39f35b24bc0fc02727113
rafzoio <raphaelzoio@hotmail.com>
2023-07-09 20:24:44 +0100

    Added link colour change to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import { React, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
+  const [activeLink, setActiveLink] = useState("");
+
+  const handleNavLinkClick = (event) => {
+    setActiveLink(event.target.innerText);
+  };
+
   return (
     <div>
       <div className="flex flex-col gap-2 p-6 text-2xl">
-        <Link to="/">
+        <Link
+          to="/"
+          className={activeLink === "Home" ? " text-white" : "text-gray-400"}
+          onClick={handleNavLinkClick}
+        >
           <h2>Home</h2>
         </Link>
-        <Link to="/library">
+        <Link
+          to="/library"
+          className={activeLink === "Library" ? " text-white" : "text-gray-400"}
+          onClick={handleNavLinkClick}
+        >
           <h2>Library</h2>
         </Link>
       </div>
