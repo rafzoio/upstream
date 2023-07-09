2ecf185bcb3d97215221753e1bb4bb3b004182d1
rafzoio <raphaelzoio@hotmail.com>
2023-07-09 19:38:10 +0100

    Added library link

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,9 +4,12 @@ import { Link } from "react-router-dom";
 const Sidebar = () => {
   return (
     <div>
-      <div className="flex flex-col gap-2 p-6">
+      <div className="flex flex-col gap-2 p-6 text-2xl">
         <Link to="/">
-          <h2 className="text-white font-karla">Home</h2>
+          <h2>Home</h2>
+        </Link>
+        <Link to="/library">
+          <h2>Library</h2>
         </Link>
       </div>
     </div>
