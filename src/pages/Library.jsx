aaf964f00b7e4ab190c8634f1515dbec62036387
rafzoio <raphaelzoio@hotmail.com>
2023-08-25 13:35:14 +0100

    Added artist text to controls

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -2,9 +2,11 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import List from "../components/List";
+import Spinner from "../components/Spinner";
 
 const Library = () => {
   const [songs, setSongs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const dispatch = useDispatch();
 
   const fetchAllSongs = async () => {
@@ -13,6 +15,7 @@ const Library = () => {
         "https://cjaaog8vtk.execute-api.eu-west-2.amazonaws.com/default/GetAllSongs"
       );
       setSongs(response.data);
+      setIsLoading(false);
     } catch (error) {
       console.error(error);
     }
@@ -33,11 +36,7 @@ const Library = () => {
     });
   }, [dispatch, songs]);
 
-  return (
-    <div>
-      <List data={songs} />
-    </div>
-  );
+  return <div>{isLoading ? <Spinner /> : <List data={songs} />}</div>;
 };
 
 export default Library;
