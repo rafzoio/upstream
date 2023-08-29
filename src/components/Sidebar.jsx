9cb8c4bde109b542b6366fc6ca5ef23d2fd22000
rafzoio <raphaelzoio@hotmail.com>
2023-08-29 11:57:53 +0100

    Removed switch from sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,41 +1,35 @@
 import { React } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-  const dispatch = useDispatch();
   const activeLink = useSelector((state) => state.activeLink);
-  const bitrate = useSelector((state) => state.bitrate);
 
-  const handleChange = () => {
-    dispatch({
-      type: "UPDATE_BITRATE",
-    });
-  }
-
-    return (
+  return (
     <div>
       <div className="flex flex-col just gap-3 p-6 text-2xl">
-          <Link
-            to="/"
-            className={activeLink === "Home" ? " text-white" : "text-gray-400"}
-          >
-            <h2>Home</h2>
-          </Link>
-          <Link
-            to="/library"
-            className={activeLink === "Library" ? " text-white" : "text-gray-400"}
-          >
-            <h2>Library</h2>
-          </Link>
-          <Link
-            to="/upload"
-            className={activeLink === "Upload" ? " text-white" : "text-gray-400"}
-          >
-            <h2>Upload</h2>
-          </Link>
-        </div>
+        <Link
+          to="/"
+          className={activeLink === "Home" ? " text-white" : "text-gray-400"}
+        >
+          <h2>Home</h2>
+        </Link>
+        <Link
+          to="/library"
+          className={activeLink === "Library" ? " text-white" : "text-gray-400"}
+        >
+          <h2>Library</h2>
+        </Link>
+        <Link
+          to="/settings"
+          className={
+            activeLink === "Settings" ? " text-white" : "text-gray-400"
+          }
+        >
+          <h2>Settings</h2>
+        </Link>
       </div>
+    </div>
   );
 };
 export default Sidebar;
