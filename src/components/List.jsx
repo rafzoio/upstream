367b48491ff16c4b5c3336a3b6a8c967b47fcc43
rafzoio <raphaelzoio@hotmail.com>
2023-08-29 11:41:38 +0100

    Added react table

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import { useTable } from "react-table";
 import { ReactComponent as PlayIcon } from "../resources/icons/play.svg";
 
 const List = ({ data }) => {
@@ -15,28 +16,77 @@ const List = ({ data }) => {
     });
   };
 
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "",
+        accessor: "play",
+        width: "10%",
+        Cell: ({ row }) => (
+          <div className="flex flex-row items-center">
+            <button onClick={() => handleSongSelect(row.original)}>
+              <PlayIcon className="w-6 fill-gray-500 hover:fill-gray-800" />
+            </button>
+          </div>
+        ),
+      },
+      {
+        Header: "Title",
+        accessor: "title",
+        width: "45%",
+      },
+      {
+        Header: "Artist",
+        accessor: "artist",
+        width: "45%",
+      },
+    ],
+    []
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      columns,
+      data,
+    });
+
   return (
-    <div>
-      <table className="w-fit text-left">
-        <thead className="text-bold text-xl ">
-          <tr>
-            <th className="pl-2">Title</th>
+    <table {...getTableProps()} className="min-w-full text-left table-auto">
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr
+            {...headerGroup.getHeaderGroupProps()}
+            className="border-gray-200 border-b-4 text-gray-800  text-left text-xs leading-4 font-medium uppercase tracking-wider"
+          >
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()} className="px-6 py-3">
+                {column.render("Header")}
+              </th>
+            ))}
           </tr>
-        </thead>
-        <tbody>
-          {data.map((song) => (
-            <tr key={song.id}>
-              <td className=" flex flex-row items-center">
-                <button onClick={() => handleSongSelect(song)}>
-                  <PlayIcon className="w-6 fill-gray-500 hover:fill-gray-800" />
-                </button>
-                {song.title}
-              </td>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()} className="bg-white">
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()} className="text-gray-700 text-sm">
+              {row.cells.map((cell) => {
+                return (
+                  <td
+                    {...cell.getCellProps()}
+                    className="px-6 py-4 whitespace-no-wrap"
+                    style={{ width: cell.column.width }}
+                  >
+                    {cell.render("Cell")}
+                  </td>
+                );
+              })}
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
 
