bb13cb45ec99c2ed17826e15ce4e29db75d2f191
rafzoio <raphaelzoio@hotmail.com>
2023-08-29 11:57:42 +0100

    Removed switch from header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,13 +8,7 @@ import { ReactComponent as LogoIcon } from "../resources/icons/waves.svg";
 
 const Header = () => {
   const dispatch = useDispatch();
-  const bitrate = useSelector((state) => state.bitrate);
-
-  const handleChange = () => {
-    dispatch({
-      type: "UPDATE_BITRATE",
-    });
-  }
+  const highQuality = useSelector((state) => state.highQuality);
 
   const handleNavLinkClick = (linkText) => {
     dispatch({
@@ -33,10 +27,6 @@ const Header = () => {
         <LogoIcon className="scale-95" />
         <h1 className="font-karla text-5xl text-gray-300">UPSTREAM</h1>
       </Link>
-      <div className="flex flex-row gap-3">
-        <h2 className="text-xl">High quality:</h2>
-      <Switch onChange={handleChange} checked={bitrate} checkedIcon={false} uncheckedIcon={false}/>
-      </div>
     </div>
   );
 };
